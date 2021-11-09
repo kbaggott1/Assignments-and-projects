972ec08f006eb47e78c433df811d1d122c84659c
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-09 12:41:34 -0500

    Create Controller.cs

---
+++ Year1/GameLab5/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLab5
+{
+    class Controller
+    {
+        public Controller ()
+        {
+             int x = 0, y = 0;
+        }
+
+        private const string sprite = "x";
+        private bool enablecontrols = false;
+
+
+        public bool EnableControls
+        {
+            get { return enablecontrols; }
+            set { enablecontrols = value; Controls(); }
+        }
+
+        public string Sprite
+        {
+            get { return sprite; }
+        }
+
+
+        private void Controls()
+        {
+            while (enablecontrols == true) 
+            {
+                Console.WriteLine("Hello");
+            }
+
+        }
+        
+    }
+}
