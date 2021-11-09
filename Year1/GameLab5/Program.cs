197d1f07a16ce72e094912262ef5437dcf0f50bf
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-09 12:42:18 -0500

    New lab started

---
+++ Year1/GameLab5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+namespace GameLab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameover = false;
+            Controller Player = new Controller();
+
+            Player.EnableControls = true;
+            
+
+            while (!gameover)
+            {
+                
+            }
+            
+        }
+    }
+}
